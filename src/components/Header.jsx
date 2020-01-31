44028859ef5ceb4fa4534f7f297faf88b1eca865
Dmitriy <brahman21122012@gmail.com>
2020-01-31 20:10:21 +0300

    add new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+return <header className='header'>
+            <img src="https://www.seoclerk.com/pics/309849-1YYJWA1419839518.png" alt="горы"/>
+           </header>;
+}
+
+export default Header;
